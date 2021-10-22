13ef90bc9efa1ced15ea5d882e1556d9954ee213
szukacs <szukacs.gergely@gmail.com>
2021-10-22 20:56:09 +0200

    questions uploaded

--- backend/src/main/java/hu/titok/junctionx/domains/Question.java
+++ backend/src/main/java/hu/titok/junctionx/domains/Question.java
@@ -2,23 +2,26 @@ package hu.titok.junctionx.domains;
 
 import hu.titok.junctionx.domains.enums.CancerType;
 import hu.titok.junctionx.domains.enums.Symptom;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Question {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Enumerated(EnumType.STRING)
     private CancerType cancerType;
+    @Enumerated(EnumType.STRING)
     private Symptom symptom;
     private String description;
 
