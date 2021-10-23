b1fc268bed84e5cfe5d0ca6e4f5acb1a55a6cac0
szukacs <szukacs.gergely@gmail.com>
2021-10-23 21:11:57 +0200

    graph added

--- frontend/src/pages/PatientHomePage/PatientHomePage.tsx
+++ frontend/src/pages/PatientHomePage/PatientHomePage.tsx
@@ -21,7 +21,7 @@ export const PatientHomePage: React.FC = () => {
 
   const [checked, setChecked] = useState(true)
 
-  const { data: test} = useQuery('bloodpressure', () => api.users.getPatient(1))
+  const { data: test } = useQuery('bloodpressure', () => api.users.getPatient(1))
 
   if (!test) {
     return <CircularProgress />
@@ -30,13 +30,13 @@ export const PatientHomePage: React.FC = () => {
   console.log(carePlamFormList);
 
   const userValues = carePlamFormList?.map(data => ({
-     date: new Date(data.dateOfSubmit!).toLocaleDateString(), 
-     diastolic: data.diastolic,
-     systolic: data.systolic,
-     weight: data.weight
-     }))
+    date: new Date(data.dateOfSubmit!).toLocaleDateString(),
+    diastolic: data.diastolic,
+    systolic: data.systolic,
+    weight: data.weight
+  }))
 
-     console.log(userValues);
+  console.log(userValues);
   if (!data && data !== null) {
     return <CircularProgress />
   }
@@ -64,10 +64,16 @@ export const PatientHomePage: React.FC = () => {
           </Stack>
         )}
         <Stack spacing={1}>
-          <LineChart width={800} height={250} data={userValues} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+          <Typography variant="h6">Active follow-up</Typography>
+          <FollowUpItem title="Second follow-up" deadline="2021. 10. 24." />
+        </Stack>
+        <Stack spacing={1}></Stack>
+        <Stack spacing={1}>
+          <Typography variant="h6">Health data</Typography>
+          <LineChart width={70 * 16} height={250} data={userValues} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="date" />
-            <YAxis />
+            <YAxis tickCount={45} />
             <Tooltip />
             <Legend />
             <Line type="monotone" dataKey="diastolic" stroke="#2832b8" />
@@ -76,26 +82,23 @@ export const PatientHomePage: React.FC = () => {
           </LineChart>
         </Stack>
         <Stack spacing={1}>
-          <Typography variant="h6">Active follow-up</Typography>
-          <FollowUpItem title="Second follow-up" deadline="2021. 10. 24." />
-        </Stack>
-        <Stack spacing={1}></Stack>
-      </Stack>
-      <Stack spacing={1}>
           <Typography variant="h6">Care Team Contact Information</Typography>
           <Stack direction="row">
-          <Stack flex={1} >
-            <Stat label="Team members " value="Dr. Ashok Vaid"/>
-            <Stat label="" value="Dr. Suresh H Advani"/>
-            <Stat label="" value="Dr. PP Bapsy"/> 
-            <Stat label="" value="Dr. Vinod Raina"/>
-          </Stack>
-          <Stack flex={1} spacing={3}>
-            <Stat label="Department phone number  " value="+36 30 6669999" />
-            <Stat label="Email address  " value="oncology_4@superclinic.net" />
-          </Stack>
+            <Stack flex={1} >
+              <Stat label="Team members " value="Dr. Ashok Vaid" />
+              <Stat label="" value="Dr. Suresh H Advani" />
+              <Stat label="" value="Dr. PP Bapsy" />
+              <Stat label="" value="Dr. Vinod Raina" />
+            </Stack>
+            <Stack flex={1} spacing={3}>
+              <Stat label="Department phone number  " value="+36 30 6669999" />
+              <Stat label="Email address  " value="oncology_4@superclinic.net" />
+            </Stack>
           </Stack>
         </Stack>
+      </Stack>
+
+
     </PatientPageBase>
   )
 }
