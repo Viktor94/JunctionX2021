8f0c468cf5c6f1d3c0a78743f79536fbcf8f715c
Marcell Szukács <spiglebach@gmail.com>
2021-10-23 11:11:55 +0200

    fix answer repository

--- backend/src/main/java/hu/titok/junctionx/repositories/AnswerRepository.java
+++ backend/src/main/java/hu/titok/junctionx/repositories/AnswerRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
-	int countAnswersByResponseAndPatientIdAndQuestionSymptomType(boolean response, SymptomType symptomType);
+	int countAnswersByResponseAndPatientIdAndQuestionSymptomType(boolean response, long patientId, SymptomType symptomType);
 }
